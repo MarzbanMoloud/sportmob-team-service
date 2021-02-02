ee2c48842f6897277ba80c1bc501840695d6611d
Hamid Seyedi <h.seyedi@tgbsco.com>
2021-02-02 12:36:08 +0330

    bug fixed  - improve serializer

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -12,8 +12,11 @@ use App\Services\EventStrategy\Interfaces\EventInterface;
 use Illuminate\Support\ServiceProvider;
 use Sentry\SentrySdk;
 use SportMob\Translation\Client;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 use Symfony\Component\Serializer\Serializer;
@@ -32,12 +35,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-    	$this->app->bind('Serializer', function (){
-			$encoders = [ new JsonEncoder()];
-            $normalizers = [new PropertyNormalizer(null,null,new ReflectionExtractor()), new DateTimeNormalizer()];
-
-			return new Serializer($normalizers, $encoders);
-		});
+        $this->app->bind('Serializer', function (){
+            $encoders = [ new JsonEncoder()];
+            $extractor = new PropertyInfoExtractor([], [new PhpDocExtractor(), new ReflectionExtractor()]);
+            $normalizers = [new PropertyNormalizer(null,null,$extractor), new DateTimeNormalizer(), new ArrayDenormalizer(),];
+            return new Serializer($normalizers, $encoders);
+        });
 
     	$this->app->singleton('TranslationClient', function (){
            return (new Client('http://127.0.0.1', 'redis', 6379, SentrySdk::getCurrentHub()));
