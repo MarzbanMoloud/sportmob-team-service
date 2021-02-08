b7369b9755176e1b68b23a25259d930f44694716
Hamid Seyedi <h.seyedi@tgbsco.com>
2021-02-08 15:06:03 +0330

    bug fixed - inject serializer

--- api/app/Listeners/Consumer/BrokerMediatorListener.php
+++ api/app/Listeners/Consumer/BrokerMediatorListener.php
@@ -7,7 +7,7 @@ namespace App\Listeners\Consumer;
 use App\Events\Consumer\BrokerMediatorEvent;
 use App\Services\EventStrategy\EventContext;
 use App\ValueObjects\Broker\Mediator\Message;
-
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Class BrokerMediatorListener
@@ -15,12 +15,26 @@ use App\ValueObjects\Broker\Mediator\Message;
  */
 class BrokerMediatorListener
 {
+    private SerializerInterface $serializer;
+    private EventContext $eventContext;
+
+    /**
+     * BrokerMediatorListener constructor.
+     * @param SerializerInterface $serializer
+     * @param EventContext $eventContext
+     */
+    public function __construct(SerializerInterface $serializer, EventContext $eventContext)
+    {
+        $this->serializer   = $serializer;
+        $this->eventContext = $eventContext;
+    }
+
     /**
      * @param BrokerMediatorEvent $event
      */
     public function handle(BrokerMediatorEvent $event)
     {
-        $messageValueObject = app('Serializer')->deserialize($event->message->getBody(), Message::class, 'json');
-        (new EventContext())->handle($messageValueObject);
+        $messageValueObject = $this->serializer->deserialize($event->message->getBody(), Message::class, 'json');
+        $this->eventContext->handle($messageValueObject);
     }
 }
--- api/app/Listeners/Consumer/BrokerQueryListener.php
+++ api/app/Listeners/Consumer/BrokerQueryListener.php
@@ -7,7 +7,7 @@ namespace App\Listeners\Consumer;
 use App\Events\Consumer\BrokerQueryEvent;
 use App\Services\BrokerQueryStrategy\BrokerQueryContext;
 use App\ValueObjects\Broker\CommandQuery\Message;
-
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Class BrokerQueryListener
@@ -15,12 +15,27 @@ use App\ValueObjects\Broker\CommandQuery\Message;
  */
 class BrokerQueryListener
 {
+
+    private SerializerInterface $serializer;
+    private BrokerQueryContext $queryContext;
+
+    /**
+     * BrokerQueryListener constructor.
+     * @param SerializerInterface $serializer
+     * @param BrokerQueryContext $queryContext
+     */
+    public function __construct(SerializerInterface $serializer, BrokerQueryContext $queryContext)
+    {
+        $this->serializer   = $serializer;
+        $this->queryContext = $queryContext;
+    }
+
     /**
      * @param BrokerQueryEvent $event
      */
     public function handle(BrokerQueryEvent $event)
     {
-        $commandQuery = app('Serializer')->deserialize($event->message->getBody(), Message::class, 'json');
-        (new BrokerQueryContext())->handle($commandQuery);
+        $commandQuery = $this->serializer->deserialize($event->message->getBody(), Message::class, 'json');
+        $this->queryContext->handle($commandQuery);
     }
 }
