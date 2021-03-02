ed52e42c67ae44a382e8511ff0a1004daf3ebd7a
Hamid Seyedi <h.seyedi@tgbsco.com>
2021-03-02 14:29:49 +0330

    bug fixed - remove default value from config

--- api/config/aws.php
+++ api/config/aws.php
@@ -1,30 +1,30 @@
 <?php
 
 return [
-    "account"  => env( "AWS_ACCOUNT_ID", '123456789100' ),
+    "account"  => env( "AWS_ACCOUNT_ID" ),
     "dynamoDb" => [
         'credentials' => [
-            'key'    => env( 'AWS_ACCESS_KEY_ID', 'local_dynamodb' ),
-            'secret' => env( 'AWS_SECRET_ACCESS_KEY', 'local_dynamodb' ),
+            'key'    => env( 'AWS_ACCESS_KEY_ID' ),
+            'secret' => env( 'AWS_SECRET_ACCESS_KEY' ),
         ],
-        'region'      => env( 'AWS_REGION', 'us-west-1' ),
+        'region'      => env( 'AWS_REGION' ),
         'version'     => 'latest',
-        'endpoint'    => env( 'AWS_DYNAMODB_ENDPOINT', 'http://dynamodb:8000' ),
+        'endpoint'    => env( 'AWS_DYNAMODB_ENDPOINT' ),
     ],
     'sns'      => [
-        'region'      => env( 'AWS_REGION', 'us-west-2' ),
+        'region'      => env( 'AWS_REGION'  ),
         'version'     => env( 'AWS_VERSION', 'latest' ),
         "credentials" => [
-            "key"    => env( 'AWS_ACCESS_KEY_ID', 'local' ),
-            "secret" => env( 'AWS_SECRET_ACCESS_KEY', 'local' )
+            "key"    => env( 'AWS_ACCESS_KEY_ID'  ),
+            "secret" => env( 'AWS_SECRET_ACCESS_KEY' )
         ],
     ],
     'sqs'      => [
-        'region'      => env( 'AWS_REGION', 'us-west-2' ),
+        'region'      => env( 'AWS_REGION' ),
         'version'     => env( 'AWS_VERSION', 'latest' ),
         "credentials" => [
-            "key"    => env( 'AWS_ACCESS_KEY_ID', 'local' ),
-            "secret" => env( 'AWS_SECRET_ACCESS_KEY', 'local' )
+            "key"    => env( 'AWS_ACCESS_KEY_ID' ),
+            "secret" => env( 'AWS_SECRET_ACCESS_KEY' )
         ],
     ]
 ];
