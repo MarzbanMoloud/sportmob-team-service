6e51fcc6f7515dc9ffd2296b2950da3f313f849b
Moloud Marzban <m.marzban@tgbs.co>
2021-03-09 17:09:13 +0330

    uncomment redis prefix

--- api/config/database.php
+++ api/config/database.php
@@ -20,7 +20,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-//            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'Lumen'), '_')),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'Lumen'), '_')),
         ],
 
         'default' => [
