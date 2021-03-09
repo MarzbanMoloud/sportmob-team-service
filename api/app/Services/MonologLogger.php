5b34192b26bf9072701557875415b3734b671a73
Hamid Seyedi <h.seyedi@tgbsco.com>
2021-03-09 12:09:24 +0330

    bug fixed - remove context prefix

--- api/app/Services/MonologLogger.php
+++ api/app/Services/MonologLogger.php
@@ -11,15 +11,8 @@ class MonologLogger extends Logger
     public function alert($message, array $context = []): void
     {
         $Sentry = app( HubInterface::class );
-        $EditedContext  = array_combine(
-            array_map( function($k) {
-                return sprintf( "%s_%s", strtolower(env( 'APP_NAME' )), $k );
-            },
-                array_keys( $context ) ),
-            $context
-        );
         try {
-            parent::alert( $message, $EditedContext );
+            parent::alert( $message, $context );
         } catch (\Exception $e) {
             $Sentry->captureException( $e->getPrevious() );
         }
