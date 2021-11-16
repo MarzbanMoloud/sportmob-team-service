97b1cab59037b125e8e1b7ef3a68d4135ffa9820
Moloud Marzban <m.marzban@tgbs.co>
2021-11-16 11:59:29 +0330

    add some fields to FAvoriteCompetiion and TopStatPerson objects

--- api/app/ValueObjects/Response/TopStatPersonResponse.php
+++ api/app/ValueObjects/Response/TopStatPersonResponse.php
@@ -11,19 +11,23 @@ namespace App\ValueObjects\Response;
 class TopStatPersonResponse
 {
 	private PersonResponse $person;
+	private string $clubTeamName;
 	private ?StatItemResponse $stat = null;
 
 	/**
 	 * @param PersonResponse $person
+	 * @param string $clubTeamName
 	 * @param StatItemResponse|null $stat
 	 * @return TopStatPersonResponse
 	 */
     public static function create(
 		PersonResponse $person,
+		string $clubTeamName,
 		?StatItemResponse $stat = null
     ): TopStatPersonResponse {
         $instance = new self();
         $instance->person = $person;
+        $instance->clubTeamName = $clubTeamName;
         $instance->stat = $stat;
         return $instance;
     }
@@ -35,6 +39,7 @@ class TopStatPersonResponse
 	{
 		return array_filter([
 			'person' => $this->person->toArray(),
+			'clubTeamName' => $this->clubTeamName,
 			'stat' => $this->stat ? $this->stat->toArray() : null,
 		]);
 	}
